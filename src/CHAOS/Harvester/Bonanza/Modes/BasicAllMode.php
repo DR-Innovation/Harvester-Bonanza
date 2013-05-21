4e671a53a95af6c6333d3444c36abdbd506fdb97
Kræn Hansen <post@creen.dk>
2013-05-21 13:12:56 +0200

    Parsing the assets in a streamlike fassion.

--- src/CHAOS/Harvester/Bonanza/Modes/BasicAllMode.php
+++ src/CHAOS/Harvester/Bonanza/Modes/BasicAllMode.php
@@ -9,21 +9,42 @@ class BasicAllMode extends \CHAOS\Harvester\Modes\AllMode implements \CHAOS\Harv
 		/* @var $bonanza \bonanza\BonanzaClient */
 		$bonanza = $this->_harvester->getExternalClient('bonanza');
 		
-		$m = 1;
+		$a = 1;
+		$assets_count = 0;
 		
 		$this->_harvester->info("Fetching references to all assets.");
-		$assets = $bonanza->GetEverything();
-		$this->_harvester->info("Found %u assets.", count($assets));
-		
-		foreach($assets as $asset) {
-			printf("[%u/%u] ", $m++, count($assets));
-			$assetShadow = null;
-			try {
-				$assetShadow = $this->_harvester->process('asset', $asset);
-			} catch(\Exception $e) {
-				$this->_harvester->registerProcessingException($e, $asset, $assetShadow);
+
+		// $assets = $bonanza->GetEverything();
+		// This is too heavy on memory - We need to parse the results as they come in.
+		// $this->_harvester->info("Found %u assets.", count($assets));
+
+		$step = new \DateInterval('P1M');
+	
+		$today = new \DateTime();
+		$limitDateBegin = new \DateTime('2010-01-01T00:00:00');
+		$limitDateEnd = clone $limitDateBegin;
+		$limitDateEnd->add($step);
+	
+		$result = array();
+	
+		do {
+			$response = $bonanza->GetDataByDates($limitDateBegin, $limitDateEnd);
+			if($response->count() > 0) {
+				$assets_count += count($response->Asset);
+				printf("Found %u Bonanza Assets in %s.\n", count($response->Asset), $limitDateBegin->format('F Y'));
+				foreach($response->Asset as $asset) {
+					printf("[%u/%u] ", $a++, $assets_count);
+					$assetShadow = null;
+					try {
+						$assetShadow = $this->_harvester->process('asset', $asset);
+					} catch(\Exception $e) {
+						$this->_harvester->registerProcessingException($e, $asset, $assetShadow);
+					}
+					print("\n");
+				}
 			}
-			print("\n");
-		}
+			$limitDateBegin->add($step);
+			$limitDateEnd->add($step);
+		} while($limitDateEnd < $today);
 	}
 }
